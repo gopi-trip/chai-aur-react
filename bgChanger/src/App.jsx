a817e1b42e68bbda89db5c5772e07ff6f2f5d002
gopi-trip <gopitrip.gt123@gmail.com>
2024-09-24 13:22:41 +0530

    complete

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    let [color,setColor] = useState("black")
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+      <div className='w-max bg-slate-200 m-auto px-3 py-2 rounded-md flex gap-2 '>
+        <button className="px-3 py-1 rounded-md text-black bg-red-500" onClick={() => setColor("red")}>Red</button>
+        <button className="px-3 py-1 rounded-md text-white bg-black" onClick={() => setColor("black")}>Black</button>
+        <button className="px-3 py-1 rounded-md text-black bg-orange-400" onClick={() => setColor("brown")}>Chocolate</button>
+        <button className="px-3 py-1 rounded-md text-black bg-purple-800" onClick={() => setColor("purple")}>Purple</button>
+        <button className="px-3 py-1 rounded-md text-black bg-pink-900" onClick={() => setColor("pink")}>Pink</button>
+        <button className="px-3 py-1 rounded-md text-black bg-yellow-300" onClick={() => setColor("yellow")}>Yellow</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+      
     </>
   )
 }
